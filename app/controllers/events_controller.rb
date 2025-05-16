4d1cb0b6d38dd8085e6e29679fc14fb096cd7d7e
RunTheLi <lanmarock@gmail.com>
2025-05-16 13:33:23 +0700

    feat:add events event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,23 @@ class EventsController < ApplicationController
   def index
     @event = Event.all
   end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to event_path, notice: "Event created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
+  end
 end
